da0bc68d3b26dfd7d97f61046d284afb0f169f6d
eunji9807 <eunji98@kissoft.co.kr>
2022-12-11 15:27:13 +0900

    - 날씨정보 추가

---
+++ NongminNews/WeatherViewer/WeatherViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  NongminTest
+//
+//  Created by 박은지 on 2022/11/24.
+//
+
+import UIKit
+import CoreLocation
+
+class WeatherViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+                
+        self.navigationItem.backButtonTitle = "날씨정보"
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    
+}
+
